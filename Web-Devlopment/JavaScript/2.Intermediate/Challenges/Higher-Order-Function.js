4d1e097b356d3371a4a27770c32d4f21064a2264
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-09-29 15:38:17 +0530

    update

--- Web-Devlopment/JavaScript/2.Intermediate/Challenges/Higher-Order-Function.js
+++ Web-Devlopment/JavaScript/2.Intermediate/Challenges/Higher-Order-Function.js
@@ -1,10 +1,26 @@
 // challenge: 1
-function addTwo(num){
+function addTwo(num) {
     return num + 2;
 }
 
 // challenge: 2
-function adds(word){
+function adds(word) {
     return word + 's'
 }
 
+// challenge: 3
+function map(array, callbackFunction) {
+
+    let temp = [];
+
+    for (let i = 0; i < array.length; i++) {
+        temp.push(callbackFunction(array[i]))
+    }
+
+    return temp;
+}
+
+const multipleByTwo = (num) => num * 2;
+
+const result = map([1, 2, 3, 4], multipleByTwo)
+console.log(result)
