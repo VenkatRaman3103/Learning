e69fe93d573558f85856d821f3c8e8193677e08f
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-09-29 15:59:54 +0530

    update

--- Web-Devlopment/JavaScript/2.Intermediate/Challenges/Higher-Order-Function.js
+++ Web-Devlopment/JavaScript/2.Intermediate/Challenges/Higher-Order-Function.js
@@ -22,5 +22,20 @@ function map(array, callbackFunction) {
 
 const multipleByTwo = (num) => num * 2;
 
-const result = map([1, 2, 3, 4], multipleByTwo)
-console.log(result)
+const challenge3 = map([1, 2, 3, 4], multipleByTwo)
+console.log(challenge3)
+
+// challenge: 4
+function forEach(array, callbackFunction) {
+    for (let i = 0; i < array.length; i++) {
+        updateString(array[i])
+    }
+}
+
+let string = ''
+
+const updateString = (char) => string += char
+
+forEach(['a', 'b', 'c', 'd'], updateString)
+
+console.log(string)
