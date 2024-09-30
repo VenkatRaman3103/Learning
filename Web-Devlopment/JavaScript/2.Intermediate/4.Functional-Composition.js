567750e125e59c2de9ebb3154a0d217e4923855e
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-09-30 20:34:33 +0530

    update

--- Web-Devlopment/JavaScript/2.Intermediate/4.Functional-Composition.js
+++ Web-Devlopment/JavaScript/2.Intermediate/4.Functional-Composition.js
@@ -19,7 +19,7 @@ const result2 = multiple1(result1)
 const result3 = subtract1(result2)
 
 
-// functional Composition
+// functional Composition with reducer
 
 const add = (num) => num + 2
 const multiple = (num) => num * 2
@@ -28,13 +28,13 @@ const subtract = (num) => num - 2
 function reduce(arrayOfFunctions, reducer, buildingUp) {
 
     for (let i = 0; i < arrayOfFunctions.length; i++) {
-        buildingUp = reducer(arrayOfFunctions[i], buildingUp)
+        buildingUp = reducer(buildingUp, arrayOfFunctions[i])
     }
 
     return buildingUp
 }
 
-const passParamToFunction = (fn, param) => fn(param)
+const passParamToFunction = (param, fn) => fn(param)
 
 const accumulator = 12;
 
@@ -42,6 +42,8 @@ const functionalComposition = reduce([
     add,
     multiple,
     subtract
-], passParamToFunction, accumulator);
+],
+    passParamToFunction, accumulator
+);
 
 console.log(functionalComposition)
