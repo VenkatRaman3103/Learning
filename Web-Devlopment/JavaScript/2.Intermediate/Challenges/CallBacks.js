c891a8407d787b2d5ff062ae3d2c6ca8138d4e75
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-10-12 16:53:17 +0530

    Update

--- Web-Devlopment/JavaScript/2.Intermediate/Challenges/CallBacks.js
+++ Web-Devlopment/JavaScript/2.Intermediate/Challenges/CallBacks.js
@@ -73,15 +73,62 @@ console.log('challenge6: ', challenge6)
 // challenge: 7
 const intersection = (arrays) => {
 
-    let repeatedElements =arrays[0]
+    let repeatedElements = arrays[0]
 
-    for(let i = 1; i < arrays.length; i++){
-        repeatedElements = repeatedElements.filter((element)=>arrays[i].includes(element))
+    for (let i = 1; i < arrays.length; i++) {
+        repeatedElements = repeatedElements.filter((element) => arrays[i].includes(element))
     }
 
     return repeatedElements
 }
 
-
 const challenge7 = intersection([[1, 2, 3, 4], [1, 5, 2, 3, 8], [1, 2, 10, 11]])
 console.log('challenge7:', challenge7)
+
+const set = {}
+
+if (set[0]) {
+    console.log('hello world')
+}
+
+// challenge: 8
+function union(arrays) {
+    let hashTable = {}
+
+    let nonRepeatingElement = []
+
+    // for (let i = 0; i < arrays.length; i++) {
+    //     const array = arrays[i]
+
+    //     for (let j = 0; j < array.length; j++) {
+    //         const element = array[j]
+
+    //         if(!nonRepeatingElement.includes(element)){
+    //             nonRepeatingElement.push(element)
+    //         }
+    //     }
+    // }
+
+    const result = arrays.reduce((acc, array) => {
+        array.forEach((element) => {
+            let isExist = false
+
+            for (let i = 0; i < acc.length; i++) {
+                if (element == acc[i]){
+                    isExist = true
+                }
+            }
+
+            if(!isExist){
+                acc.push(element)
+            }
+        })
+        return acc
+    }, [])
+
+    return result
+}
+
+const challenge8 = union([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]])
+console.log('challenge8', challenge8)
+
