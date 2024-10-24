40eb54a48bd31f440526848f0c73efa7f2979d62
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-10-24 23:27:59 +0530

    update

---
+++ Rough/rough.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int counter = 0;
+
+    vector<int> filterdArr = {};
+
+    for (int i = 0; i < nums.size(); i++) {
+      int element = nums[i];
+      if (element == val) {
+        counter++;
+      } else {
+        filterdArr.push_back(element);
+      }
+    }
+
+    return counter;
+  };
+};
+
+int main() {
+
+};
--- rough.cpp
+++ rough.cpp
@@ -3,31 +3,34 @@
 
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-		vector<int> nonZeroes = {};
+  void moveZeroes(vector<int> &nums) {
+    vector<int> nonZeroes = {};
 
-		int zeroCounter = 0;
-		
-		for(int i = 0; i < nums.size();i++){
-			int element = nums[i];
+    int zeroCounter = 0;
 
-			if(element != 0){
-				nonZeroes.push_back(element);
-			}else{
-				zeroCounter++;
-			};
-		}
+    for (int i = 0; i < nums.size(); i++) {
+      int element = nums[i];
 
-		for(int j = 0; j <zeroCounter;j++){
-			nonZeroes.push_back(0);
-		}
-
-		for(int k = 0; k < nums.size(); k++){
-			nums[k] = nonZeroes[k];
-		}
+      if (element != 0) {
+        nonZeroes.push_back(element);
+      } else {
+        zeroCounter++;
+      };
     }
+
+    for (int j = 0; j < zeroCounter; j++) {
+      nonZeroes.push_back(0);
+    }
+
+    for (int k = 0; k < nums.size(); k++) {
+      nums[k] = nonZeroes[k];
+    }
+  }
 };
 
-int main(){
+int main() {}
 
-}
+// {
+//   "tabWidth": 4,
+//   "useTabs": true
+// }
