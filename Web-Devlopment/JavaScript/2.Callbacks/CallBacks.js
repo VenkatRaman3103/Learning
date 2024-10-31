1be31e63d1219710c6a250a2addde345930c4b21
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-10-31 18:20:40 +0530

    updat

--- Web-Devlopment/JavaScript/2.Callbacks/CallBacks.js
+++ Web-Devlopment/JavaScript/2.Callbacks/CallBacks.js
@@ -67,6 +67,53 @@ const reduce = (array, callbackFunction, initialValue) => {
 
 const reducer = (num) => num + 2;
 
+return {
+    "folke/zen-mode.nvim",
+    opts = {
+        window = {
+            backdrop = 1,
+            width = 80,
+            height = 1,
+            options = {
+                relativenumber = true,
+            },
+            backdrop_hlgroup = "ZenBg",
+        },
+        plugins = {
+            options = {
+                enabled = true,
+                ruler = false,
+                showcmd = false,
+                laststatus = 0,
+            },
+            twilight = { enabled = true },
+            gitsigns = { enabled = false },
+            tmux = { enabled = false },
+            todo = { enabled = false },
+            wezterm = { enabled = false },
+        },
+        on_open = function()
+            vim.fn.system("tmux set status off") -- Hide tmux status bar
+        end,
+        on_close = function()
+            vim.fn.system("tmux set status on") -- Restore tmux status bar
+        end,
+    },
+    config = function(_, opts)
+        require("zen-mode").setup(opts)
+        -- Set the keybinding, here we're using <leader>z as an example
+        vim.keymap.set("n", "<leader>zm", function()
+            require("zen-mode").toggle()
+        end, { desc = "Toggle Zen Mode" })
+
+        -- Add an autocmd to restore tmux status bar when Neovim exits
+        vim.api.nvim_create_autocmd("VimLeavePre", {
+            callback = function()
+                vim.fn.system("tmux set status on")
+            end,
+        })
+    end,
+}
 const challenge6 = reduce([1, 2, 3, 4, 5], reducer, 0);
 console.log("challenge6: ", challenge6);
 
