7f59b53f73a590d1d3964bcf40a43365d1d1e035
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-11-15 21:54:20 +0530

    update

--- Web-Devlopment/JavaScript/Deep-JavaScript-Foundations/1.1-Types/1.1-types.js
+++ Web-Devlopment/JavaScript/Deep-JavaScript-Foundations/1.1-Types/1.1-types.js
@@ -1,3 +1,5 @@
+// using utilit called typeof
+
 let v;
 
 typeof v; // currently has no value, means it is not defined, a state
@@ -30,5 +32,29 @@ const str = "hello world";
 
 if (!typeof str == undefined) {
 } // type coercion
+
 if (!typeof str == "undefined") {
 } // right way of comparing
+
+let variable = null; // object
+console.log(typeof variable);
+
+variable = function () {}; // function
+console.log(typeof variable);
+
+variable = [1, 2, 3]; // object
+console.log(typeof variable);
+
+let bigInteger = 42n;
+console.log(typeof bigInteger);
+
+let greet = "hello world";
+greet = undefined;
+console.log(typeof greet);
+
+let user = {
+	name: "foo",
+	address: "bar",
+};
+user = null;
+console.log(typeof user);
---
+++ Web-Devlopment/JavaScript/Deep-JavaScript-Foundations/Rough/rough.js
@@ -0,0 +1,9 @@
+let variable = 10;
+
+let getType = typeof variable;
+
+console.log(getType === "number");
+console.log(typeof getType === "string");
+
+console.log(isNaN(12));
+console.log(isNaN("string"));
