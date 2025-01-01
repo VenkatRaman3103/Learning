2c60528d0e50c993840f86c5a48c2811ba718a3f
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-01 20:45:30 +0530

    update

--- Programming-Languages/Python/1.4.Conditions/conditions.py
+++ Programming-Languages/Python/1.4.Conditions/conditions.py
@@ -1,3 +1,6 @@
+import math
+
+
 # question 1
 def get_grade(score):
     grade = "A"
@@ -29,3 +32,34 @@ def cost_of_project(engraving, solid_gold):
 
 
 print(cost_of_project("venkat", True))
+
+
+# question 3
+def get_water_bill(num_gallons):
+    if num_gallons <= 8000:
+        bill = (num_gallons / 1000) * 5
+    elif num_gallons <= 22000:
+        bill = (num_gallons / 1000) * 6
+    elif num_gallons <= 30000:
+        bill = (num_gallons / 1000) * 7
+    else:
+        bill = (num_gallons / 1000) * 10
+    return bill
+
+
+print(get_water_bill(8000))
+
+
+# question 4
+def get_phone_bill(gb):
+
+    if gb <= 15:
+        bill = 100
+    elif gb >= 15:
+        difference = gb - 15
+        bill = round((difference * 100) + 100)
+
+    return bill
+
+
+print(get_phone_bill(15.1), "<==")
