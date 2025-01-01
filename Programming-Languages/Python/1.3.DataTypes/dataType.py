ae8cb3627594f14caee5fb7633a4ae9e338c0f86
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-01 18:38:32 +0530

    update

--- Programming-Languages/Python/1.3.DataTypes/dataType.py
+++ Programming-Languages/Python/1.3.DataTypes/dataType.py
@@ -10,3 +10,32 @@ wholeNumber = int(integer)
 print("str" * 0)  # print emty string
 print(type("str"))  # <class 'str'>
 print(type("str" * 12))  # <class 'str'>
+
+
+# question 3
+def get_expected_cost(beds, baths, has_basement):
+    baseAmount = 80000
+
+    value = (beds * 30000) + (baths * 10000) + (has_basement * 40000) + baseAmount
+
+    return value
+
+
+print(get_expected_cost(2, 1, True))
+
+# question 4
+print(False + False)  # 0
+print(True + False)  # 1
+print(False + True)  # 1
+print(True + True)  # 2
+print(False + True + True + True)  # 3
+
+
+def cost_of_project(engraving, solid_gold):
+    cost = ((solid_gold * 100) + ((len(engraving) * solid_gold) * 10)) + (
+        not solid_gold * 50 + ((len(engraving) * solid_gold) * 7)
+    )
+    return cost
+
+
+print(cost_of_project("venkat", False), "<==")
