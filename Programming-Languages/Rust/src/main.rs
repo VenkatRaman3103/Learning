677fe4e6391e64abb12cd095691bd5bad1118ea2
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-03-28 22:10:48 +0530

    update

--- Programming-Languages/Rust/src/main.rs
+++ Programming-Languages/Rust/src/main.rs
@@ -1,5 +1,7 @@
-mod collection;
-mod patterns;
+// mod collection;
+// mod module;
+// mod patterns;
+mod pattern_practice;
 
 fn main() {
     // collections
@@ -8,5 +10,12 @@ fn main() {
     // collection::structs();
 
     // pattern matching
-    patterns::enums();
+    // patterns::enums();
+    // patterns::patter_matching();
+
+    // pattern matching practice
+
+    pattern_practice::basics::day_of_week(8); // 1. day of week
+    pattern_practice::basics::match_grade(90); // 2. match grade
+    pattern_practice::basics::is_even(3); // 3. is even
 }
--- Programming-Languages/Rust/src/patterns.rs
+++ Programming-Languages/Rust/src/patterns.rs
@@ -1,19 +1,19 @@
-enum WorldLiterature {
-    Russia(String),
-    India(indian_writer),
-    Europe,
-}
-
-struct indian_writer {
-    jeyamohan: String,
-    sundara_ramasamy: String,
-    basheer: String,
-}
-
 pub fn enums() {
+    enum WorldLiterature {
+        Russia(String),
+        India(IndianWriter),
+        Europe,
+    }
+
+    struct IndianWriter {
+        jeyamohan: String,
+        sundara_ramasamy: String,
+        basheer: String,
+    }
+
     let my_favorite = WorldLiterature::Russia("Hello world".to_string());
 
-    let indian_author_works: indian_writer = indian_writer {
+    let indian_author_works: IndianWriter = IndianWriter {
         jeyamohan: String::from("aram"),
         sundara_ramasamy: String::from("Oru puliyamarathing kathai"),
         basheer: String::from("mathilgal"),
@@ -25,12 +25,45 @@ pub fn enums() {
         println!("{}", s);
     }
 
-    if let WorldLiterature::India(indian_writer {
+    if let WorldLiterature::India(IndianWriter {
         jeyamohan,
         sundara_ramasamy,
         basheer,
     }) = favorite_indian_author
     {
         println!("{}, {}, {}", jeyamohan, sundara_ramasamy, basheer)
+    }
+}
+
+pub fn patter_matching() {
+    enum Syntax {
+        Integer(i32),
+        Variables(TokenKind),
+    }
+
+    struct TokenKind {
+        token_value: String,
+        token_type: String,
+    }
+
+    let string_token: TokenKind = TokenKind {
+        token_value: String::from("var"),
+        token_type: String::from("string"),
+    };
+
+    let string_token_type: Syntax = Syntax::Variables(string_token);
+
+    let integer_token: Syntax = Syntax::Integer(12);
+
+    match string_token_type {
+        Syntax::Variables(TokenKind {
+            token_type,
+            token_value,
+        }) => {
+            println!("{} - {}", token_value, token_type)
+        }
+        Syntax::Integer(i) => {
+            println!("{}", i)
+        }
     }
 }
