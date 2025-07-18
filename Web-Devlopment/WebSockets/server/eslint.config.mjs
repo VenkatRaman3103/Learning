9539e96fc7ab1b900ddbbd7abc2f5ebd907c0b0e
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-07-18 22:16:32 +0530

    update

---
+++ Web-Devlopment/WebSockets/server/eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: {
+            parser: tsParser,
+            globals: globals.node,
+        },
+        plugins: { "@typescript-eslint": tseslint },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            ...tseslint.configs.recommended.rules,
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { argsIgnorePattern: "^_" },
+            ],
+            "no-console": "off",
+        },
+    },
+];
