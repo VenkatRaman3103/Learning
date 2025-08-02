157916defee4beb3f6db272107d8e26f3d0c4f7a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-08-02 08:34:23 +0530

    update

--- Function-Programming/FirstStep/src/practice/answers.js
+++ Function-Programming/FirstStep/src/practice/answers.js
@@ -1,5 +1,20 @@
 const arr = [1, 2, 3, 4, 5];
 
+/// utils
+const head = arr => {
+    return arr[0];
+};
+
+const tail = arr => {
+    return arr.slice(1);
+};
+
+const concat = (arr_1, arr_2) => {
+    return [...arr_1, ...arr_2];
+};
+
+///
+
 const doubleTheValues = (fn, arr) => {
     if (arr.length == 0) {
         return [];
@@ -15,16 +30,24 @@ const mulByTwo = value => {
     return value * 2;
 };
 
-const head = arr => {
-    return arr[0];
+// console.log(doubleTheValues(mulByTwo, arr));
+
+const filterEvens = (fn, arr) => {
+    if (arr.length == 0) {
+        return [];
+    }
+
+    const item = head(arr);
+
+    if (fn(item)) {
+        return concat([item], filterEvens(fn, tail(arr)));
+    } else {
+        return filterEvens(fn, tail(arr));
+    }
 };
 
-const tail = arr => {
-    return arr.slice(1);
+const isEven = value => {
+    return value % 2 == 0;
 };
 
-const concat = (arr_1, arr_2) => {
-    return [...arr_1, ...arr_2];
-};
-
-console.log(doubleTheValues(mulByTwo, arr));
+console.log(filterEvens(isEven, arr));
